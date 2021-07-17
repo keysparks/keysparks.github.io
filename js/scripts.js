02c8afb57a09a60a8071dcd787c71508f96fa81d
keysparks <83922152+keysparks@users.noreply.github.com>
2021-07-17 10:33:10 +0530

    thanks message

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,16 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
-function al() {
-    var email = document.getElementById("emailid").value;
-    mootrack('identify', email, 'keysparks');
+function submitRequest() {
+    const InputTextBox = document.getElementById("emailid");
+    mootrack('identify', InputTextBox.value, 'keysparks');
+    const button = document.getElementById('submit-button');
+    const thanksDiv = document.getElementById('thanksMessage');
+    disableButton = true;
+    if (disableButton) {
+        button.disabled = "disabled";
+        button.innerText = "Subscribed!";
+        InputTextBox.value = "";
+        thanksDiv.style = "display: block"
+    } else { button.disabled = false; thanksDiv.style = "display: none" };
 };
